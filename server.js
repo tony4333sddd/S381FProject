0b6457d058e627d9350e65250c18caa2ffe0dacb
tony4333sddd <tommygor2020@gmail.com>
2024-11-06 10:16:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,65 +1,134 @@
 const express = require('express');
 const session = require('cookie-session');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 const SECRETKEY = 'I want to pass COMPS381F';
-
-const users = new Array(
-	{name: 'admin', password: '123'},
-	{name: 'guest', password: '123'}
-);
-
-app.set('view engine','ejs');
 
 app.use(session({
   name: 'loginSession',
   keys: [SECRETKEY]
 }));
 
-// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-	console.log(req.session);
-	if (!req.session.authenticated) {    // user not logged in!
-		res.redirect('/login');
-	} else {
-		res.status(200).render('loggedAdmin',{name:req.session.username});
-	}
+mongoose.connect('mongodb+srv://hkmudata:Ch070401x7@cluster0.0902v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const todoSchema = new mongoose.Schema({
+  task: String,
+  description: String,
+  completed: Boolean
+});
+const ToDo = mongoose.model('ToDo', todoSchema);
+
+app.get('/', (req, res) => {
+  res.redirect('/login');
 });
 
-app.get('/login', (req,res) => {
-	res.status(200).render('login',{});
+app.get('/login', (req, res) => {
+  res.status(200).render('login', {});
 });
 
-
-app.post('/login', (req,res) => {
-	users.forEach((user) => {
-		if (user.name == req.body.name && user.password == req.body.password) {
-			// correct user name + password
-			// store the following name/value pairs in cookie session
-			req.session.authenticated = true;        // 'authenticated': true
-			req.session.username = req.body.name;	 // 'username': req.body.name		
-		}
-	});
-	res.redirect('/');
+app.post('/login', (req, res) => {
+  const users = [{ name: 'admin', password: '123' }];
+  users.forEach((user) => {
+    if (user.name === req.body.name && user.password === req.body.password) {
+      req.session.authenticated = true;
+      req.session.username = req.body.name;
+    }
+  });
+  res.redirect('/loggedAdmin');
 });
 
-
-
-app.get('/logout', (req,res) => {
-	req.session = null;   // clear cookie-session
-	res.redirect('/login');
+app.get('/loggedAdmin', (req, res) => {
+  if (req.session.authenticated) {
+    res.render('loggedAdmin', { name: req.session.username });
+  } else {
+    res.redirect('/login');
+  }
 });
 
-app.on('error', (err) => {
-    console.error('Server error:', err);
+app.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/login');
+});
+
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await ToDo.find({});
+    res.render('todos', { todos });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/createTodo', (req, res) => {
+  const newTodo = new ToDo({
+    task: req.body.task,
+    description: req.body.description,
+    completed: false
+  });
+  newTodo.save().then(() => res.redirect('/todos'));
+});
+
+app.get('/editTodo/:id', async (req, res) => {
+  try {
+    const todo = await ToDo.findById(req.params.id);
+    res.render('editTodo', { todo });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/updateTodo/:id', async (req, res) => {
+  try {
+    await ToDo.findByIdAndUpdate(req.params.id, {
+      task: req.body.task,
+      description: req.body.description,
+      completed: req.body.completed === 'on'
+    });
+    res.redirect('/todos');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/deleteTodo/:id', async (req, res) => {
+  try {
+    await ToDo.findByIdAndDelete(req.params.id);
+    res.redirect('/todos');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/searchTodos', async (req, res) => {
+  try {
+    const query = {};
+    if (req.query.query) {
+      query.task = new RegExp(req.query.query, 'i');
+    }
+    if (req.query.completed) {
+      query.completed = req.query.completed === 'true';
+    }
+    const todos = await ToDo.find(query);
+    res.render('todos', { todos });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(8099, () => {
-    console.log('Server is running on port 8099');
+  console.log('Server is running on port 8099');
 });
